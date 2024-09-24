69594b24f86a4a1f6697e740775ace3a2a4a4dae
sensitiky <mariomcorrea3@gmail.com>
2024-09-24 01:54:56 -0300

    models updates

--- models/userModel.go
+++ models/userModel.go
@@ -1,12 +1,12 @@
 package models
 
 type User struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	LastName string `json:"lastName"`
-	Email    string `json:"email"`
-	Avatar   string `json:"avatar"`
-	Password string `json:"password"`
+	Id       int     `json:"id"`
+	Name     string  `json:"name"`
+	LastName string  `json:"lastName"`
+	Email    string  `json:"email"`
+	Avatar   *string `json:"avatar"`
+	Password string  `json:"password"`
 }
 
 type LoginUser struct {
